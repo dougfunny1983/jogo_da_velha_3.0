a29553d0d18c574e4d4c22da943b2ba7a96d44e3
Douglas Eduardo <douglaseduardo220483@gmail.com>
2020-03-13 17:35:32 -0300

    git

--- jogo_da_velha/src/reducers/reducer.js
+++ jogo_da_velha/src/reducers/reducer.js
@@ -1,20 +1,26 @@
 import { GET_A_CLICK, RELOADER } from '../actions/action';
 
 export const intialState = { squares: Array(9).fill(null), selected: true };
+
 export const reducer = (state = intialState, action) => {
+
   switch (action.type) {
+
     case GET_A_CLICK:
       return {
         ...state,
         squares: [...state.squares],
         selected: !state.selected,
       };
+
       case RELOADER:
       return {
         squares: Array(9).fill(null),
         selected: true,
       };
+
     default:
       return state;
   }
+  
 };
